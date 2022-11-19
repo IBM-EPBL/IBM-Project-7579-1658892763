d0497534b525a4249130de0e97336d3809cb16d7
Harini A <73424245+harini002@users.noreply.github.com>
2022-11-19 23:48:41 +0530

    Add files via upload

---
+++ Final Deliverables/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+from PIL import Image
+import numpy as np
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+
+app = Flask(__name__, template_folder="templates/templates")
+
+model = load_model('models/mnistCNN.h5')
+print("Loaded model from disk")
+
+@app.route('/')
+def upload_file():
+    return render_template('Home.html')
+@app.route('/Upload')  
+def upload_file1():
+    return render_template('Upload.html')
+@app.route('/Draw')
+def upload_file2():
+    return render_template('Draw.html')
+@app.route('/Guidelines')
+def upload_file3():
+    return render_template('Guidelines.html')
+
+@app.route('/Predict',methods=['POST'])
+def upload_image_file():
+   if request.method=='POST':
+     if request.files["file"]:
+       img = Image.open(request.files['file'].stream).convert("L")
+       img = img.resize((28,28))
+       im2arr = np.array(img)
+       im2arr = im2arr.reshape(1,28,28,1)
+       y_pred = np.argmax(model.predict(im2arr))
+       if(y_pred >= 0 and y_pred < 10):
+        return render_template("Upload.html", showcase = str(y_pred))
+     else :
+        return render_template("Upload.html")
+        
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 8000, debug = True)
+
+    
